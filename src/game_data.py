dc958d1652a26f5977e62a74c586bcd3a2038b6b
loerssoni <lauri.heikka@gmail.com>
2024-11-29 10:00:28 +0000

    data wrangling + modularize

---
+++ src/game_data.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+# from sheets import read_data, schedule_sheets_update, request_game_data
+import sheets
+
+def read_players():
+    range_name = 'NHL!A1:T'  
+    return sheets.read_data(range_name)
+
+def read_teams():
+    range_name = 'JOUKKUEET!A1:B'  
+    return sheets.read_data(range_name)
+
+def read_schedule():
+    range_name = 'AIKATAULU!A1:D'
+    
+    return sheets.read_data(range_name).dropna()
+
+def read_players():
+    range_name = 'NHL!A1:T'  
+    return sheets.read_data(range_name)
+
+
+def get_scoreboard(games, goals):
+    scores = goals.groupby(['scoring_team', 'SARJA', 'KOTI', 'VIERAS', 'name'], as_index=False)['n_goals'].max()
+    scores = scores.pivot(columns='scoring_team', index='name', values='n_goals')
+
+    if len(goals) > 0:
+        scoreboard = games.join(scores, on=['name'], rsuffix='_SCORE')
+    else:
+        scoreboard = games
+        scoreboard['KOTI_SCORE'] = 0
+        scoreboard['VIERAS_SCORE'] = 0
+
+    scoreboard[['KOTI_SCORE', 'VIERAS_SCORE']] = scoreboard[['KOTI_SCORE', 'VIERAS_SCORE']].fillna(0)
+    scoreboard['game_n'] = ''
+    scoreboard.loc[~scoreboard['SARJA'].str.contains('lohko'), 'game_n'] = 'Playoffs (Game ' + scoreboard['name'].str.split('_').str[1] + '.)'
+    scoreboard['series'] = scoreboard['name'].str[:-2]
+    scoreboard.loc[scoreboard.series.str.endswith('lohko'), 'series'] = scoreboard.loc[scoreboard.series.str.endswith('lohko'), 'series'].str[-7:]
+    scoreboard['winner'] = scoreboard.apply(
+        lambda row: row['KOTI'] if row['KOTI_SCORE'] > row['VIERAS_SCORE'] else 
+                    (row['VIERAS'] if row['VIERAS_SCORE'] > row['KOTI_SCORE'] else 'TIE'), 
+        axis=1
+    )
+    return scoreboard
+
+def get_standings(scoreboard):    # Transform to long-form and count victories as before
+    long_df = pd.melt(
+        scoreboard.loc[scoreboard.game_state == 'PÄÄTTYNYT'], 
+        id_vars=["series", "winner", "KOTI_SCORE", "VIERAS_SCORE"], 
+        value_vars=["KOTI", "VIERAS"],
+        value_name="team"
+    )
+    
+    long_df['win'] = long_df['team'] == long_df['winner']
+    long_df['tie'] = long_df['winner'] == 'TIE'
+    long_df['loss'] = (~long_df['win'])&(~long_df['tie'])
+    long_df['goalsfor'] = 0
+    long_df.loc[long_df.variable == 'KOTI', 'goalsfor'] += long_df.loc[long_df.variable == 'KOTI', 'KOTI_SCORE']
+    long_df.loc[long_df.variable == 'VIERAS', 'goalsfor'] += long_df.loc[long_df.variable == 'VIERAS', 'VIERAS_SCORE']
+    long_df['goalsaga'] = 0
+    long_df.loc[long_df.variable == 'KOTI', 'goalsaga'] += long_df.loc[long_df.variable == 'KOTI', 'VIERAS_SCORE']
+    long_df.loc[long_df.variable == 'VIERAS', 'goalsaga'] += long_df.loc[long_df.variable == 'VIERAS', 'KOTI_SCORE']
+    long_df['games'] = 1
+    standings = long_df.groupby(['series','team'], as_index=False)[['games', 'win','tie','loss']].sum()
+    standings['points'] = standings['win'] * 2 + standings['tie']
+
+    base = scoreboard[['series','KOTI']]
+    base.columns = ['series','team']
+    base2 = scoreboard[['series','VIERAS']]
+    base2.columns = base.columns
+    base = pd.concat([base, base2]).drop_duplicates()
+    final = base.merge(standings, how='left', on=['series','team'])
+    
+    final =  final.fillna(0).sort_values(['series', 'points', 'win','games'], ascending=[True, False, False, True]).reset_index(drop=True)
+    final['position'] = final.groupby('series').cumcount()  + 1
+    return final
+
+def get_players(goals, teams):
+    players = read_players()
+    points_data = {}
+    points_data['goal_scorers'] = goals.scorer.value_counts()
+    points_data['assist1'] = goals.ass_1.value_counts()
+    points_data['assist2'] = goals.ass_2.value_counts()
+    points_data = pd.DataFrame(points_data).fillna(0)
+    points_data['assists'] = points_data[['assist1','assist2']].sum(1)
+    points_data['points'] = points_data[['goal_scorers','assist1','assist2']].sum(1)
+    points_data = points_data.loc[points_data.index != '']
+    players = players.join(points_data, on='Player')
+    players[points_data.columns] = players[points_data.columns].fillna(0)
+    players = players.merge(teams, left_on='Team', right_on='Joukkue', how='inner')
+    return players
+
+class GameData():
+    def __init__(self):
+        sheets.schedule_sheets_update('create')
+        self.refresh_data()
+    
+    def refresh_data(self):
+        self.teams = read_teams()
+        self.schedule = read_schedule()
+        self.games, self.goals = sheets.read_game_data(self.schedule)
+        self.scoreboard = get_scoreboard(self.games, self.goals)
+        self.standings = get_standings(self.scoreboard)
+        self.scoreboard = self.scoreboard \
+                .merge(self.standings, left_on=['KOTI', 'series'], right_on=['team','series'])\
+                .merge(self.standings, left_on=['VIERAS', 'series'], right_on=['team','series'], suffixes=('_home','_away'))
+        self.players = get_players(self.goals, self.teams)
+     
---
+++ src/sheets.py
@@ -0,0 +1,189 @@
+key_path = '../secrets/sheet-reader-key.json'
+spreadsheet_id = '1VwC-GYUuEjsZr1DqGgeo8Wkv6_Ly6NZjAV07Zeik4ko'
+
+
+import os
+import pandas as pd
+
+from apiclient import discovery
+from google.oauth2 import service_account
+
+scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
+secret_file = os.path.join(os.getcwd(), key_path)
+
+
+credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
+service = discovery.build('sheets', 'v4', credentials=credentials)
+
+def schedule_sheets_update(mode):
+    if mode not in ['create','delete']:
+        raise ValueError('Invalid mode')
+    range_name = 'AIKATAULU!A1:D100'
+    template_id = '61864045'
+    
+    schedule = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name, valueRenderOption='UNFORMATTED_VALUE').execute()
+    
+    metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    all_sheets = {p['properties']['title']:p['properties']['sheetId'] for p in metadata['sheets']}
+    
+    n_games = {}
+    reqs = []
+    
+    update_cells = []
+    scoreboard_updates = []
+    for i, row in enumerate(schedule['values'][1:]):
+        if len(row) == 4:
+            teams = sorted(row[1:3])
+            game_id = f'{teams[0]}{teams[1]}{row[0]}'
+            n_games[game_id] = n_games.get(game_id, 0) + 1
+
+            name = f'{game_id}_{n_games[game_id]}'
+            update_cells.append((name, True))
+
+            if (mode == 'create'):
+                if name not in all_sheets:
+                    reqs.append({
+                        "duplicateSheet": {
+                            "sourceSheetId": template_id,
+                            "newSheetName": name,
+                        }
+                    })
+                scoreboard_updates.append([
+                    {
+                        'range': f'{name}!A2:A2',  # Specify the first range
+                        'values': [[row[1]]]
+                    },
+                    {
+                        'range': f'{name}!F2:F2',  # Specify the first range
+                        'values': [[row[2]]]
+                    }
+                ])
+                
+            elif (mode=='delete' and name in all_sheets):
+                reqs.append({
+                    f"deleteSheet": {
+                        "sheetId": all_sheets[name]
+                    }
+                })
+        else:
+            update_cells.append(('', False))
+
+    if len(reqs) > 0:# Execute the batchUpdate request
+        body = {"requests": reqs}
+        response = service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body=body
+        ).execute()
+        
+        metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+        all_sheets = {p['properties']['title']:p['properties']['sheetId'] for p in metadata['sheets']}
+    
+   
+    cell_updates = []
+    if mode == 'create':
+        url_base = f"""=HYPERLINK(\"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/edit"""
+        names = [r['duplicateSheet']['newSheetName'] for r in reqs]
+        for name, update in update_cells:
+            if update:
+                cell_updates.append([f"""{url_base}?gid={all_sheets[name]}#gid={all_sheets[name]}", "Gamelink")"""])
+            else:
+                cell_updates.append([""])
+
+    if mode == 'delete':
+        cell_updates = [[""] for i in schedule['values']]
+    
+    body = {
+        'values': cell_updates
+    }
+
+    response = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id,
+        range="AIKATAULU!E2:E",  # Specify the range
+        valueInputOption="USER_ENTERED",
+        body=body
+    ).execute()
+    
+    if mode == 'create':
+        response = service.spreadsheets().values().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body={'data':scoreboard_updates, 'valueInputOption':'USER_ENTERED'}
+        ).execute()
+    
+    
+    move_requests = []
+    for sheet_name in ['AIKATAULU','JOUKKUEET','NHL','TEMPLATE']:
+        move_requests.append({
+        "updateSheetProperties": {
+            "properties": {
+                "sheetId": all_sheets[sheet_name],
+                "index": 0
+            },
+            "fields": "index"
+        }})
+    
+    request_body_move = {
+        "requests": [move_requests]
+    }
+    service.spreadsheets().batchUpdate(
+        spreadsheetId=spreadsheet_id,
+        body=request_body_move
+    ).execute()
+
+    return
+
+def read_game_data(schedule):
+    
+    n_games = {}
+    metadata_rows = []
+    ranges = []
+    for row in schedule.to_dict(orient='records'):
+        teams = sorted([row['KOTI'], row['VIERAS']])
+        game_id = f"{teams[0]}{teams[1]}{row['SARJA']}"
+        n_games[game_id] = n_games.get(game_id, 0) + 1
+
+        name = f'{game_id}_{n_games[game_id]}'
+        row['name'] = name
+        ranges.append(f'{name}!A5:J30')
+        metadata_rows.append(row)
+
+    # Request to batch get values from multiple ranges
+    response = service.spreadsheets().values().batchGet(
+        spreadsheetId=spreadsheet_id,
+        ranges=ranges,
+        valueRenderOption='UNFORMATTED_VALUE'
+    ).execute()
+
+    goals = []
+    for value_range, metadata in zip(response['valueRanges'], metadata_rows):
+        metadata['game_state'] = value_range['values'][23][3]
+        for i, row in enumerate(value_range.get('values', [])[:20]):
+            if len(row) > 0:
+                row_data = {
+                    'scoring_team':'KOTI',
+                    'scorer':row[0],
+                    'ass_1': row[1],
+                    'ass_2': row[2],
+                    'n_goals': i + 1
+                }
+
+                row_data.update(metadata)
+                goals.append(row_data)
+
+                row_data = {
+                    'scoring_team':'VIERAS',
+                    'scorer':row[5],
+                    'ass_1': row[6],
+                    'ass_2': row[7],
+                    'n_goals': i + 1
+                }
+                row_data.update(metadata)
+                goals.append(row_data)
+    goals = pd.DataFrame(goals)
+    goals = goals[goals.scorer != '']
+    return pd.DataFrame(metadata_rows), goals
+
+
+def read_data(range_name):
+    data = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name, valueRenderOption='UNFORMATTED_VALUE').execute()
+    return pd.DataFrame(data['values'][1:], columns=data['values'][0])
+    
